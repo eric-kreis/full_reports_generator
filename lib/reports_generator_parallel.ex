a0bcb69b190d1e4bc5168afd11465dd8b6f1c8cb
Eric Kreis <erickreis793@gmail.com>
2022-09-04 20:31:15 -0300

    starting project :rocket:

---
+++ lib/reports_generator_parallel.ex
@@ -0,0 +1,18 @@
+defmodule ReportsGeneratorParallel do
+  @moduledoc """
+  Documentation for `ReportsGeneratorParallel`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ReportsGeneratorParallel.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/reports_generator_parallel_test.exs
@@ -0,0 +1,8 @@
+defmodule ReportsGeneratorParallelTest do
+  use ExUnit.Case
+  doctest ReportsGeneratorParallel
+
+  test "greets the world" do
+    assert ReportsGeneratorParallel.hello() == :world
+  end
+end
